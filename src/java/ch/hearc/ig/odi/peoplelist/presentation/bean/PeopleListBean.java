9ab82a4b027eaeb309bc45ad696037da2807b90b
sergio.dasilvad <sergio.dasilvad@IG13-DASILVADS.intra.eiaj.ch>
2016-10-21 15:10:22 +0200

    Insertion de la balise Inject

--- src/java/ch/hearc/ig/odi/peoplelist/presentation/bean/PeopleListBean.java
+++ src/java/ch/hearc/ig/odi/peoplelist/presentation/bean/PeopleListBean.java
@@ -5,8 +5,11 @@
  */
 package ch.hearc.ig.odi.peoplelist.presentation.bean;
 
+import ch.hearc.ig.odi.peoplelist.services.Services;
 import java.util.Date;
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -17,63 +20,11 @@ import javax.inject.Named;
 @RequestScoped
 public class PeopleListBean {
 
-    private Long id;
-    private String gender;
-    private String firstName;
-    private String lastName;
-    private Boolean married;
-    private Date birthDate;
+    @Inject
+    Services services;
 
-    public PeopleListBean() {
+    public List getPeopleList() {
+        return services.getPeopleList();
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Boolean getMarried() {
-        return married;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setMarried(Boolean married) {
-        this.married = married;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-    
-    
 }
